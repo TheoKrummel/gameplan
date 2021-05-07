03a7aec0ad6a61d7ed45933be8304bb04716ef6e
TheoKrummel <tdkrummel@icloud.com>
2021-05-07 15:07:27 -0500

    Update main.js

--- Game/main.js
+++ Game/main.js
@@ -1,115 +1,265 @@
+const hiddenBot = require('./hiddenBot.js')
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-let GameState = {
-  openingSetting: 
-`"Your story begins in the once illustrious capitol city **Christallis**. 
+let GS = {
+  channel: 0,
+  openingSetting: `"Your story begins in the once illustrious capitol city **Christallis**. 
 Named for the rare crystal "Zhonflorite" only found here in all of "NohM\'Ana". 
 The city built upon great wealth, and quickly attracted many travelers looking for quick and easy money, growing in size, population, and power." \n`,
   classes: {
-    'GUNSLINGER': 'Gunslinger',
-    'WARDEN': 'Warden'
+    'GUNSLINGER':{ 
+      name:'Gunslinger',
+      BH: 105,
+      BA: 2,
+      abilities:{
+        abilityOne: {
+          name:'Headshot',
+          damage: 45,
+        },
+        abilityTwo:{
+          name:'Trickshot',
+          damage: 75,
+          CTH: 75
+        }
+      }},
+    'WARDEN': {
+      name:'Warden',
+      BH: 135,
+      BA: 13,
+      abilities:{
+        abilityOne: {
+          name:'Headbutt',
+          damage: 40,
+        },
+        abilityTwo:{
+          name:'Police Brutality',
+          damage: 70,
+          CTH: 75
+        }
+      }
+    }
   },
-  randomEncounter: {
+  RE: {
     options: ['Fight', 'Flee'],
-    list: ['Gablin Gang', 'Dreg Crew', 'Mafia Boys']
+    list: {
+      'GABLIN GANG': {
+      name:'Gablin Gang',
+      BH: 100,
+      BA: 5,
+      Attacks:{
+        BasicAttack: 15,
+        SecondaryAttack: 30
+      }
+      },
+      'DREG CREW': {
+        name: 'Dreg Crew',
+        BH: 100,
+        BA: 5,
+        Attacks:{
+          BasicAttack: 15,
+          SecondaryAttack: 30
+        }
+      },
+      'MAFIA BOYS': {
+        name:'Mafia Boys',
+        BH: 100,
+        BA: 5,
+        Attacks:{
+          BasicAttack: 15,
+          SecondaryAttack: 30
+        }
+      }
+    } 
   },
-  playerState: {
+  PS: {
+    name: 'Default',
+    qIndex: 0,
     class: 'N/A',
-    HP: 100
+    HP: 0,
+    lives: 3,
+  },
+  ES: {
+    actions: []
   }
 }
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  GS.channel = client.channels.cache.find(channel => channel.name === 'test-channel')
 });
 
-client.login("ODI2MTQzNTQ5NDMwMTY5NjA2.YGIMIw.YFbDt75LRHMYVHpJHXUEzXssUzk");
+client.login(hiddenBot.hiddenBot);
 
-const classPick = (msg, classChoice) => {
-  GameState.playerState.class = GameState.classes[classChoice]
-  msg.channel.send(`You chose ${GameState.playerState.class}.`)
+const openingConversation = (answerChoice) => {
+  const conversationChoice = answerChoice
+  if(conversationChoice === '1'){
+    GS.channel.send(`\n"Suck it up ${GS.PS.name}, there\'s trouble brewin\' my friend."`)
+    GS.channel.send(`"Do you think you're ready to do this? Out there, you\'ll be on your own."\n1: Do what? Where am I?! \n2: YEah I'm READY \n`)
+    GS.PS.qIndex = 1;
+  }else if(conversationChoice === '2'){
+    GS.channel.send('\n"That\'s good. I doubt it will last."')
+    GS.channel.send(`(You see a faint smile on his lips. He washes a glass and stares down at the bar table)\n "It\'s almost time to go ${GS.PS.name}."\n1: So it is. What will you do?\n2: Go where?\n`)
+    GS.PS.qIndex = 2
+  }else{console.log('shut up')}
+  
+}
+
+const openingConversation1 = (answerChoice) => {
+  const conversationChoice1 = answerChoice
+    if(conversationChoice1 === '1'){
+      GS.channel.send('\n(He chuckles)\n"You always know how to make people smile. Good luck out there."')
+      GS.channel.send('You give the old man a nod, take a deep breath and walk out of the bar.')
+      }else if(conversationChoice1 === '2'){
+      GS.channel.send('\n(Your enthusiasm brings a small smile to his face)\n"With that attitude I know we can change things around here. Good luck out there."')
+      GS.channel.send('You give the old man a nod, take a deep breath and walk out of the bar.')
+      }else{
+      console.log('shut up inside q11')
+      }
+      fightEncounter()
+  }
+
+  const openingConversation2 = (answerChoice) => {
+    const conversationChoice2 = answerChoice
+    if(conversationChoice2 === '1'){
+      GS.channel.send(`\n"Me? I'll be here. The Mafia won't touch me. Even if they tried, Charlie would rip them a new one."\n(He jerks his thumb towards a tommygun mounted on the wall behind his head)\n"I wish you the best of luck. You\'ll need it.`)
+      GS.channel.send('You give the old man a nod, take a deep breath and walk out of the bar.')
+    }else if(conversationChoice2 === '2'){
+      GS.channel.send(`\n"That\'s for you to find out and for me to watch. Hope you\'re ready for what\'s out there."`)
+      GS.channel.send('You give the old man a nod, take a deep breath and walk out of the bar.')
+    }else{console.log('shut up inside q12')}
+    fightEncounter()
+  }
+
+const classPick = (classChoice, rebirth) => {
+  GS.PS.HP = GS.classes[classChoice].BH
+  if(rebirth === false){ 
+    GS.PS.class = GS.classes[classChoice].name.toUpperCase()
+    GS.channel.send(`You chose ${GS.PS.class}.`)
+    GS.channel.send(`\n(You're in an almost empty bar, save for the bartender, an old man wearing the outfit of a theater usher.)\n "How do you feel?" He asks. \n 1: Pretty shit.\n2: Great!\n`)
+  }
 }
 
 client.on("message", (msg) => {
-  somethingElse = msg;
+
+  GS.PS.name = msg.author;
   let splitMessage = msg.content.split(" ");
 
   let gayMessage = splitMessage.indexOf('gay');
   if (gayMessage > -1){
-    msg.channel.send('Your mom is gay, friggin idiot');
+    GS.channel.send('Your mom is gay, friggin idiot');
   }
-  // SCUFFED EVERYTHING I CAN ONLY GET IT TO WORK HERE GONNA NEED HELP ON THIS ONE
+
   let startMessage = splitMessage.indexOf('START');
   if(startMessage > -1){
-    msg.channel.send('Game is starting. Get Ready!')
-    msg.channel.send(GameState.openingSetting)
-    msg.channel.send('What class will you choose?' + `\n Gunslinger \n Warden \n`)
+    GS.channel.send('Game is starting. Get Ready!')
+    // GS.channel.send(GS.openingSetting)
+    GS.channel.send('What class will you choose?' + `\n\n Gunslinger \n Warden \n`)
   }
   let classMessage = splitMessage.indexOf('-class');
   if(classMessage === 0){
-    classPick(msg, splitMessage[1].toUpperCase())
+    classPick(splitMessage[1].toUpperCase(), false)
+  }
+  
+  let madeChoice = splitMessage.indexOf('-choice');
+  if(madeChoice === 0){
+    let convo = GS.PS.qIndex;
+    let choice = splitMessage[1];
+    if(convo === 0){
+      openingConversation(splitMessage[1])
+    } 
+    else if(convo === 1){
+      if(choice === '1'){
+        openingConversation1(splitMessage[1])
+      }else if (choice === '2'){
+        openingConversation1(splitMessage[1])
+      }
+    }else if(convo === 2){
+      if(choice === '1'){
+        openingConversation2(splitMessage[1])
+      }else if(choice === '2'){
+        openingConversation2(splitMessage[1])
+      }
+    }
+  }
+
+  let admin = splitMessage.indexOf('-admin');
+  if(admin === 0){
+    if(splitMessage[1] === 'reset') {
+      GS.PS.qIndex = 0
+      GS.PS.class = 'N/A'
+      GS.channel.send('Game is starting. Get Ready!')
+      GS.channel.send(GS.openingSetting)
+      GS.channel.send('What class will you choose?' + `\n Gunslinger \n Warden \n`)
+    }
+  }
+
+  let continueMessage = splitMessage.indexOf('-continue')
+        if(continueMessage === 0){
+          classPick(GS.PS.class, true)
+          GS.channel.send(`You Revive`)
+          GS.PS.lives = GS.PS.lives - 1
+          console.log(GS.PS.HP)
+          console.log(GS.PS.lives)
+        }
+
+  let fightOptionsMessage = splitMessage.indexOf('-ability')
+  if(fightOptionsMessage === 0){
+    const playerArmor = GS.classes[GS.PS.class].BA
+    const ability = splitMessage[1]
+    
+    GS.classes[GS.PS.class].abilities
+    const abilityOne = GS.classes[GS.PS.class].abilities.abilityOne
+    const abilityTwo = GS.classes[GS.PS.class].abilities.abilityTwo
+    if(ability === '1'){
+      GS.ES.actions.push(abilityOne.name)
+      GS.ES.encounter.BH = GS.ES.encounter.BH - abilityOne.damage
+      const lastAbility = GS.ES.actions[GS.ES.actions.length -1]
+      GS.channel.send(`You used ${lastAbility}. ${GS.ES.encounter.name}'s health has been reduced to ${GS.ES.encounter.BH}`)
+    }else if(ability === '2'){
+      GS.ES.actions.push(abilityTwo.name)
+      const chance = Math.random()
+      if(chance > .99){
+        GS.ES.encounter.BH = GS.ES.encounter.BH - abilityTwo.damage
+        const lastAbility = GS.ES.actions[GS.ES.actions.length -1]
+        GS.channel.send(`You used ${lastAbility}. ${GS.ES.encounter.name}'s health has been reduced to ${GS.ES.encounter.BH}`)
+      }else if(chance < .99){
+        GS.channel.send('The attack missed!')
+      }
+    }
+    if(GS.ES.encounter.BH < 1){
+      GS.channel.send(`YOU MURDERED THEM OH MY GOD WHAT THE FUCK IS WRONG WITH YOU \nthanks for playing lol`)
+    }
+    if(GS.ES.encounter.BH > 1){ 
+      const enemyAttacks = Object.keys(GS.ES.encounter.Attacks)
+      const attackGenerator = Math.floor(Math.random()*2)
+      const theBigAttack = enemyAttacks[attackGenerator]
+      GS.PS.HP =  GS.PS.HP - (GS.ES.encounter.Attacks[theBigAttack] - playerArmor)
+      if(GS.PS.HP < 1){
+        GS.channel.send(`Wow. That was fast.\n **YOU DIED**\n Continue?`)
+        
+      }
+      if(GS.PS.HP > 1){
+      GS.channel.send(`${GS.ES.encounter.name} clobbered your fuckin face in bud, dealing ${GS.ES.encounter.Attacks[theBigAttack] - playerArmor} damage!\n Your new HP is ${GS.PS.HP}\n What will you do? \n1: ${abilityOne.name}\n2: ${abilityTwo.name}`)
+    }
+    }
   }
 })
 
-const createRandEnc = () => {
-  
-  const rand = Math.floor(Math.random()*3)
-  const enc = GameState.randomEncounter.list[rand]
-  
-  const randomEnc = {
-    encounter: enc,
-    options: GameState.randomEncounter.options
-  }
-  
-  const question = `You've encountered a ${randomEnc.encounter}!\nWhat will you do?`
-  const options = `\n 1: ${randomEnc.options[0]} \n 2: ${randomEnc.options[1]}\n`
+const createRandEnc = function (obj) {
+  const keys = Object.keys(GS.RE.list);
+  return GS.RE.list[keys[ keys.length * Math.random() << 0]];
+};
 
-  const resp = promptPlayer(question, options)
+const fightEncounter = () => {
+  const encounter = createRandEnc()
+  GS.channel.send(`\n(Surveying your environment, you realize you\'re surrounded by a ${encounter.name})\n Prepare yourself.`)
+  GS.ES['encounter'] = encounter
 
-  return { resp: resp, options: randomEnc.options}
+  const pClass = GS.PS.class;
+  const abilityOne = GS.classes[pClass].abilities.abilityOne.name
+  const abilityTwo = GS.classes[pClass].abilities.abilityTwo.name
+
+  GS.channel.send(`\nWhat will you do? \n1: ${abilityOne}\n2: ${abilityTwo}`)
 }
-
-
-// const classPick = () => {
-//   const classChoice = promptPlayer('What class will you choose?', `\n ${GameState.classes[0]} \n ${GameState.classes[1]} \n`)
-//   GameState.playerState.class = GameState.classes[classChoice - 1]
-//   console.log(`You chose ${GameState.playerState.class}.`)
-// }
-
-// I KNOW I CAN MAKE THIS BETTER BUT IM WORKING ON IT lol
-// Planning to make it a function like promptPlayer at some point just to make conversations easy
-// In case you see this before I'm finished, theres a bug where the text repeats itself and I'm working on it
-const openingConversation = () => {
-  const q1 = promptPlayer(`\n(You're in an almost empty bar, save for the bartender, an old man wearing the outfit of a theater usher.)\n "How do you feel?" He asks. \n`, `1: Pretty shit.\n2: Great!\n`)
-  if(q1 === '1'){
-    console.log('\n"Suck it up, there\'s trouble brewin\' my friend."')
-    const q11 = promptPlayer(`"Do you think you're ready to do this? Out there, you\'ll be on your own."\n`, `1: Do what? Where am I?! \n2: YEah I'm READY \n`)
-    if(q11 === '1'){
-    console.log('\n(He chuckles)\n"You always know how to make people smile. Good luck out there."')
-    }else if(q11 === '2'){
-      console.log('\n(Your enthusiasm brings a small smile to his face)\n"With that attitude I know we can change things around here. Good luck out there."')
-    }else{
-      console.log('shut up inside q11')
-    }
-  }else if(q1 === '2'){
-    console.log('\n"That\'s good. I doubt it will last."')
-    const q12 = promptPlayer(`(You see a faint smile on his lips. He washes a glass and stares down at the bar table)\n "It\'s almost time to go."\n`, `1: So it is. What will you do?\n2: Go where?\n`)
-    if(q12 === '1'){
-      console.log(`\n"Me? I'll be here. The Mafia won't touch me. Even if they tried, Charlie would rip them a new one."\n(He jerks his thumb towards a tommygun mounted on the wall behind his head)\n"I wish you the best of luck. You\'ll need it.`)
-    }else if(q12 === '2'){
-      console.log(`\n"That\'s for you to find out and for me to watch. Hope you\'re ready for what\'s out there."`)
-    }else{console.log('shut up inside q12')}
-  }else{console.log('shut up')}
-  console.log('You give the old man a nod, take a deep breath and walk out of the bar.')
-}
-
-// GAME STARTS HERE
-
-// Show opening scene
-// console.log(GameState.openingSetting)
-
-// Ask player to pick class
-
-// classPick()
-// openingConversation()
